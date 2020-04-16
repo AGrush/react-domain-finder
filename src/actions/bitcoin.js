80a32c99eed3ded8694a59898549a9572389d5b9
AGrush <grushevskiy@gmail.com>
2020-04-16 17:03:17 +0100

    .

---
+++ src/actions/bitcoin.js
@@ -0,0 +1,11 @@
+import { FETCH_BITCOIN } from './constants';
+
+export const fetchBitcoin = () => {
+  //dispatch wraps around our promise
+  return dispatch => { 
+    return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => response.json())
+      .then(json => dispatch({ type: FETCH_BITCOIN, bitcoin: json}))
+  }
+}
+
